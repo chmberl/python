a3464c1fb30241174bd3fb2e022adb4444d57bc1
张博良 <chmberl@gmail.com>
2015-05-19 21:48:09 +0800

    spider

---
+++ spider/doubanmovie/doubanmovie/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for doubanmovie project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'doubanmovie'
+
+SPIDER_MODULES = ['doubanmovie.spiders']
+NEWSPIDER_MODULE = 'doubanmovie.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'doubanmovie (+http://www.yourdomain.com)'
