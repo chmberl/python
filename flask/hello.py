800c5fdfcc25d7f0200ec92e36584255732f4c31
张博良 <chmberl@gmail.com>
2015-05-26 23:32:58 +0800

    flask

---
+++ flask/hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'index page'
+
+@app.route('/hello')
+def hello_world():
+    return "hello world"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
