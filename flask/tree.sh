940fbc214d86ff1e6e8aaa5ac54e72189bda5fff
张博良 <chmberl@gmail.com>
2015-06-08 21:56:21 +0800

    2015-06-08

--- flask/tree.sh
+++ flask/tree.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-dir="."
-echo $dir
-for line in`ls -lR | awk ' /^\./{print substr($1,1,length($1)-1)} /^-/{print $1,$9}'`
+function print_profix
+{
+    profix="|-"
+    for i in `seq $1`
+    do
+        profix=${profix}"-"
+    done
+    echo ${profix}
+}   
+
+
+base="."
+count=0
+echo $base
+for line in `ls -AlR | awk ' /^\./{print substr($1,1,length($1)-1)} /^-/{print $1":"$9}'`
 do
     first=${line:0:1}
     if [ $first == "." ];then
-        dir=`basename $line`
-        
+        base=`basename $line`
+        dir=`dirname $line`
+        count=${#dir}
+        profix=`print_profix $count`
+        base_count=${#base}
+        ((count+=base_count))
+    else 
+        base=`echo $line | awk -F: '{print $2}'`
+        profix=`print_profix $count`
     fi
+    echo ${profix}" "${base}
 done
 
