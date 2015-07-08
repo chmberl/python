eac7a8a4ce8d80c02b3c34ead0925b560b994274
张博良 <chmberl@gmail.com>
2015-07-08 20:21:33 +0800

    20150708

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from tutorial.quickstart.serializers import	UserSerializer, GroupSerializer
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+
 
 class UserViewSet(viewsets.ModelViewSet):
-	"""
-	API endpoint that allows users to be viewed or edited.
-	"""
-	queryset = User.object.all()
-	
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.object.all()
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import include, url
-from django.contrib import admin
-from django.http import HttpRequest
+from rest_framework import routers
+from tutorial.quickstart import views
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupsViewSet)
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'tutorial.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls',
+        namespace='rest_framework')),
 ]
